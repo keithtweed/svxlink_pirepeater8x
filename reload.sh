f4eea9152ace64444764f985e949432eedada8bc
Keith Tweed <86432205+keithtweed@users.noreply.github.com>
2025-01-07 15:11:44 -0700

    Remove verbose flags from tar and add progress messages

--- reload.sh
+++ reload.sh
@@ -19,12 +19,18 @@ SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 DATE=$(date +'%Y%m%d-%H%M%S')
 
 # Backup current RepeaterLogics
-(cd /usr/share/svxlink/events.d && tar -czvf ~/backup_events-$DATE.tar.gz RepeaterLogic?.tcl)
-(cd /etc/svxlink && tar -czvf ~/backup_svxlink-$DATE.tar.gz $(ls -A /etc/svxlink))
+echo -n "Backing up current configuration files..."
+(cd /usr/share/svxlink/events.d && tar -czf ~/backup_events-$DATE.tar.gz RepeaterLogic?.tcl)
+(cd /etc/svxlink && tar -czf ~/backup_svxlink-$DATE.tar.gz $(ls -A /etc/svxlink))
+echo " Done"
 
 # Copy the configuration files
+echo -n "Copying configuration files..."
 cp -r $SCRIPT_DIR/svxlink/* /etc/svxlink/
 cp -r $SCRIPT_DIR/events/* /usr/share/svxlink/events.d/
+echo " Done"
 
 # Restart the svxlink service
+echo -n "Restarting svxlink service..."
 systemctl restart svxlink
+echo " Done"
